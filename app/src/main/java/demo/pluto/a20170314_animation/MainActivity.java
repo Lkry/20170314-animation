2085d9669de1c065c56508be72e02e44df2fb465
cool  zy <cool  zy>
2017-03-14 22:27:04 +0800

    第一次提交一个小玫瑰

---
+++ app/src/main/java/demo/pluto/a20170314_animation/MainActivity.java
@@ -0,0 +1,19 @@
+package demo.pluto.a20170314_animation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        ImageView spaceshipImage = (ImageView) findViewById(R.id.flower);
+        Animation hyperspaceJumpAnimation = AnimationUtils.loadAnimation(this, R.anim.hyperspace_jump);
+        spaceshipImage.startAnimation(hyperspaceJumpAnimation);
+    }
+}
